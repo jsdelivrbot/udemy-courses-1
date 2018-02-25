cd358dd64cfddabdb35b7d575c6659c4da176579
Keano <keanov@gmail.com>
2018-02-25 14:50:21 -0800

    init book-list project sq-redux

---
+++ react-redux-sg/refresh/book-list/src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+const booksReducer = () => {
+    return [
+        { title: 'Javascript: the good parts' },
+        { title: 'Harry potter' },
+        { title: 'Lord of the rings' },
+        { title: 'Eloqeunt ruby' }
+    ]
+}
+
+export default booksReducer;
